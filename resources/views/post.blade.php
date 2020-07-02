6ac5329cd370679b71f24f2fdb93bc47d6fe9f90
Mészáros Ádám <meszike91@gmail.com>
2020-07-02 16:40:04 +0200

    2.4 útvonalválastás

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,4 +1,2 @@
-<!--<h1>hello word</h1>
-<h1>My blog</h1>-->
 <h1>My blog</h1>
 <p>{{ $post }}</p>
--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,4 @@ Route::get('/posts/{post}', function ($post){
     return $post;
 });*/
 
-Route::get('/posts/{post}', function ($post) {
-    $posts = [
-     'my-first-post' => 'Hello, this is my first blog post!',
-     'my-second-post' => 'Now I am getting the hang of this blogging thing'
-    ];
-
-    if ( ! array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-}
-
-return view('post', [ 
-    'post' => $posts[$post] ?? 'Nothing here yet.'
-]);
-});
+Route::get('/posts/{post}', 'PostsController@show');
