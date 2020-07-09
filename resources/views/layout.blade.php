58f75edb2b0bdc5fce3c65c9403378d39fa84189
Mészáros Ádám <meszike91@gmail.com>
2020-07-09 16:24:04 +0200

    4.4 Eszközkészlet összeállítás a Laravel Mix és webpack csomagokkal

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,7 @@ Released   : 20140225
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/app.css">
 
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
 
@@ -42,4 +43,7 @@ Released   : 20140225
 @yield('header')
 @yield('content')
 </body>
+<div id="copyright" class="container">
+	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
 </html>
